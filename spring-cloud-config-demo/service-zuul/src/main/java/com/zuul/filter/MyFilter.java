592a7f7e7a3bcc35fd7d05a9a03bb4bd8cd95781
moviewang <2314165649@qq.com>
2018-04-04 11:29:33 +0800

    zuul

---
+++ spring-cloud-config-demo/service-zuul/src/main/java/com/zuul/filter/MyFilter.java
@@ -0,0 +1,51 @@
+package com.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * Created by Movie on 2018/4/4.
+ */
+@Component
+public class MyFilter extends ZuulFilter {
+    private static final Logger logger = LoggerFactory.getLogger(MyFilter.class);
+
+    public String filterType() {
+        return "pre";
+    }
+
+    public int filterOrder() {
+        return 0;
+    }
+
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        logger.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        String token = request.getParameter("token");
+        if (Objects.isNull(token)) {
+            logger.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            } catch (IOException e) {
+                logger.error("response exception [{}], {}", e.getMessage(), e);
+            }
+        }
+        logger.info("it's ok");
+        return null;
+    }
+}
